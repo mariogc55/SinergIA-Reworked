25d90dd0bd186b2e0464f5ab722b61a69fa4058f
MGC55 <mariogc5555@gmail.com>
2025-10-22 16:24:39 -0500

    Delete SinergIA directory

--- SinergIA/frontend-vue/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-const app = createApp(App);
-
-app.mount('#app');
-
-console.log('Frontend Vue.js iniciado y conectado al MS-Orquestador (3000) y MS-Métricas (3002)');
